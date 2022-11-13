4cdf8dffea7e40f4b809bcc79f4a46288d988664
domhdev <domhdev@gmail.com>
2022-11-13 16:05:31 +1100

    stuff

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import app from firebaseConfig
+
+//Misc Helpers from youtube tute
+function randomFromArray(array){
+  return array[Math.floor(Math.random() * array.length)];
+}
+function getKeyString(x, y) {
+  return `${x}x${y}`;
+}
+// (function () {
+
+//   getAuth((user) => {
+//     console.log(user)
+//     if (user){
+
+//     } else {
+
+//     }
+//   })
+
+
+
+// })();
+
+
+
+
